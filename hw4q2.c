8b8f9712bd2c5ee8d9344c07747116371fe0c37b
Andrew Kuklinski <anku7985@colorado.edu>
2017-10-12 23:29:24 -0600

    final update

--- hw4q2.c
+++ hw4q2.c
@@ -3,28 +3,14 @@
 #include <stdlib.h>
 //1.
 #define SET_EQUAL(a,b) (a=(uint8_t)b)
-/* parentheses around the b varible like (a=(uint8_t)(b)) would unsure
-   that b always gets evaluated first*/
-
 
 //2.
 #define SQUARE(a) (a*a)
-/*macro arguements should be put in parentheses like ((a)*(a)) to avoid
-  issues like SQUARE(x+2) -> expanded to (x+2*x+2) which gives an answer
-of 41 and not the expected 255 */
-
-  /*possible issue that SQUARE does not change the 
-value of x when it is squared, must be assigned to a
-variable*/
 
 //3.
 #define BASE 16
 #define OFFSET 10+BASE
 int8_t var = OFFSET*2;
-
-/*order of operations gives a different value than what is expected 
-  Issue can be fixed by defining OFFSET as (10+BASE)*/
-
 
 void main(){
   printf("Homework #4, Question 2 - Andrew Kuklinski\n\n");
@@ -32,8 +18,10 @@ void main(){
   uint8_t x = 5;
   uint16_t y = 13;
 
+  printf("variable x and y are assigned\n");
   printf("x = %d | y = %d\n",x,y);
   SET_EQUAL(x,y);
+  printf("SET_EQUAL macro is called\n");
   printf("After SET_EQUAL: x = %d | y = %d\n",x,y);
 
   printf("x = %d | y = %d\n\n",x,y);
